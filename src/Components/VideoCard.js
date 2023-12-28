dafea4f0ed17330ec3ff3cf4e227d4b9bb7ac9e4
Rahil <rahil.das37@gmail.com>
2023-12-29 02:38:18 +0530

    add duration tag to video thumbnails

--- src/Components/VideoCard.js
+++ src/Components/VideoCard.js
@@ -1,12 +1,28 @@
 import React from 'react'
 
 const VideoCard = ({info}) => {
-    const {snippet,statistics} = info;
+    const {snippet,statistics,contentDetails} = info;
     const {thumbnails,title,channelTitle} = snippet;
     const {viewCount} = statistics;
+
+    const getVideoDuration = ()=>{
+        const durationString = contentDetails.duration;
+        //console.log(typeof(durationString))
+        const regex = /PT(?:(\d+)M)?(?:(\d+)S)?/;
+        const match = durationString.match(regex);
+        if(match){
+            const min = parseInt(match[1])||0
+            const sec = parseInt(match[2])||0;
+            return(`${min}:${sec}`)
+        }
+    }
+
     return (
         <div className='p-2 m-2 w-72 rounded-lg'>
+            <div className='relative'>
+            <span className='absolute bottom-1 right-0 text-white bg-slate-800 px-1 rounded-md'>{getVideoDuration()}</span>
             <img className='rounded-xl' src={thumbnails.medium.url} alt='thumbnail'/>
+            </div>
             <ul>
                 <li className='font-bold'>{title}</li>
                 <li className='font-light'>{channelTitle}</li>
